93e5b2e0c4e15c387fe5c53099ca5a6e8a72252d
norrisj <johntnorris@outlook.com>
2014-07-03 09:19:09 +0100

    Tied enquiry HTML to the handler, added CSS and image

--- client/views/home/enquiry_form.js
+++ client/views/home/enquiry_form.js
@@ -1,7 +1,12 @@
 Template.EnquiryForm.events({
   'submit form': function (e, tmpl) {
-    console.log("send");
     e.preventDefault();
-    Meteor.call("sendEnquiry", {from: "adamdawkins@gmail.com", message: "hello john" });
+
+    var form = tmpl.find('form'),
+      enquiry = {from: form.from.value, message: form.message.value };
+
+    console.log('EnquiryForm', form, enquiry);
+
+    Meteor.call("sendEnquiry", enquiry);
   }
 });
--- server/methods/send_mail.js
+++ server/methods/send_mail.js
@@ -1,11 +1,13 @@
 Meteor.methods({
   sendEnquiry: function (options) {
     var email = {
-      to: "johntnorris@outlook.com", 
+      to: "johntnorris@outlook.com",
       from: options.from,
       subject: "CUSTOMER ENQUIRY",
       html: options.message
     };
+
+    console.log('sendEnquiry', options);
 
     Meteor.Mandrill.send(email);
   }
